f0345fe0e9abf678f7ba614f8908b1e3c19b1889
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-10 02:10:35 +0300

    implementing ejs(embedded javascript)

--- blog/index.js
+++ blog/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.listen(4000, () => {
@@ -9,17 +12,21 @@ app.listen(4000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  //   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  //   res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
 
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  //   res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  //   res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
 });
