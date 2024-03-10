a49ef958a8eb39066a9c911dbb16b9ad39909a26
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-10 20:39:16 +0300

    create a blog post

--- blog/index.js
+++ blog/index.js
@@ -2,36 +2,41 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const BlogPost = require("./models/BlogPost");
 
 const app = express();
 
+// Set view engine and public directory
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
-});
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/blog_database");
 
-mongoose.connect("mongodb://localhost:27017/blog_database", {
-  useNewUrlParser: true,
-});
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define routes
 app.get("/", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "pages/index.html"));
-  res.render("index");
+  const blogPosts = BlogPost.find({});
+  res.render("index", blogPosts);
+});
+app.get("/about", (req, res) => res.render("about"));
+app.get("/post", (req, res) => res.render("post"));
+app.get("/contact", (req, res) => res.render("contact"));
+app.get("/posts/new", (req, res) => res.render("create"));
+
+// Handle form submission
+app.post("/posts/store", async (req, res) => {
+  await BlogPost.create(req.body);
+
+  res.redirect("/");
 });
 
-app.get("/about", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "pages/about.html"));
-  res.render("about");
-});
-
-app.get("/post", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "pages/post.html"));
-  res.render("post");
-});
-
-app.get("/contact", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "pages/contact.html"));
-  res.render("contact");
+// Start server
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
