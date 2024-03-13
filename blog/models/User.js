29013a287316a2b2581ddf5bfffe4e2b139936e2
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-14 00:28:06 +0300

    unique username validator

--- blog/models/User.js
+++ blog/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new Schema({
   username: {
@@ -12,6 +13,8 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model("User", userSchema);
 
