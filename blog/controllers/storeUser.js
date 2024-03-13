263083bc4a884f9914cadf4c5da54e39bb874c56
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-14 00:53:33 +0300

    data persistency in forms

--- blog/controllers/storeUser.js
+++ blog/controllers/storeUser.js
@@ -14,6 +14,7 @@ module.exports = async (req, res) => {
       ? Object.keys(error.errors).map((key) => error.errors[key].message)
       : [];
     req.flash("validationErrors", validationErrors);
+    req.flash('data', req.body)
     res.redirect("/auth/register");
   }
 };
