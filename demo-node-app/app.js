c2b6f501e7a0f21ae8ead2a0c0db368ef6c6cabf
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-17 01:26:31 +0300

    pug form

--- demo-node-app/app.js
+++ demo-node-app/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes/index");
 
 const app = express();
+
+// middlewares
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// routes
 app.use("/", routes);
 
 module.exports = app;
