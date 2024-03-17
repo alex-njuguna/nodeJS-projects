cd731a3ad544bdf013d5b5182b68697a299ceb27
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-17 23:26:07 +0300

    data validation

--- demo-node-app/routes/index.js
+++ demo-node-app/routes/index.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("form", { title: "Registration form" });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.render("form", { title: "Registration form" });
-});
+router.post(
+  "/",
+  [
+    body("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    body("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    // console.log(req.body);
+    if (errors.isEmpty()) {
+      res.send("Thank you for your registratiion");
+    } else {
+      res.render("form", {
+        title: "Registration form",
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
