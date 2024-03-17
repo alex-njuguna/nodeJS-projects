b849f42913702971de37a3749b35049c8ad56241
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-17 23:53:18 +0300

    Registration schema

---
+++ demo-node-app/models/Registration.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const registrationSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+});
+
+const Registration = mongoose.model("Registration", registrationSchema);
+
+module.exports = Registration;
