698155e4fbe2d054512db41e6405039203f89cc9
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-17 23:47:59 +0300

    create a database connection

--- demo-node-app/start.js
+++ demo-node-app/start.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const app = require("./app");
+const mongoose = require("mongoose");
+
+// create a database connection
+mongoose.connect(process.env.DATABASE);
 
 const server = app.listen(3000, () => {
   console.log(`Server is running on port ${server.address().port}`);
