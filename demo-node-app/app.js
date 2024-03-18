106ebe7560c7a8d6b3122afe77c3521172029f8f
alex-njuguna <njugunakinuthia013@gmail.com>
2024-03-19 01:52:47 +0300

    last nail

--- demo-node-app/app.js
+++ demo-node-app/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const routes = require("./routes/index");
+const exp = require("constants");
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 // routes
 app.use("/", routes);
